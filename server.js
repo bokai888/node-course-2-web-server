145dbda69a33f9e18451482e2bd68ab633f8ccbb
bokai888 <ktabor25@gmail.com>
2019-01-11 16:25:46 -0600

    Nav partial added and practice upload

--- server.js
+++ server.js
@@ -43,6 +43,13 @@ app.get('/', (req, res) => {
     })
 });
 
+app.get('/projects', (req, res) => {
+   res.render('projects.hbs', {
+       pageTitle: 'Projects!',
+       welcomeMessage: 'Welcome to the projects page'
+   })
+});
+
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
         pageTitle: 'About Page'
